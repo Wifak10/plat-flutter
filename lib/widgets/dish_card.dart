425d7cc6d8e9f7a29aa38ae62d6924031f8bca72
wifak10 <wifakayekpola123@gmail.com>
2025-02-13 17:52:09 +0100

    Ajout d'une barre de recherche pour filtrer les catégories et les plats, amélioration de l'affichage des cartes de plats avec un bouton "View" pour plus de détails.

--- lib/widgets/dish_card.dart
+++ lib/widgets/dish_card.dart
@@ -4,10 +4,9 @@ import 'package:flutter_svg/flutter_svg.dart';
 import '../models/category.dart';
 import '../models/dish.dart';
 import 'package:flip_card/flip_card.dart';
-// import '../widgets/search_bar.dart' as custom;
 import '../widgets/category_card.dart';
 import '../widgets/dish_card.dart';
-// import 'package:flutter/widgets/dish_card.dart'; // Replace 'your_project' with the actual project name
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -16,7 +15,6 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
   @override
   void initState() {
     super.initState();
@@ -39,7 +37,6 @@ class _HomeScreenState extends State<HomeScreen> {
     Category(title: 'Pie', iconPath: 'icons/pie.svg', color: const Color.fromARGB(255, 179, 181, 241)),
     Category(title: 'Pancakes', iconPath: 'icons/pancakes.svg', color: const Color.fromARGB(255, 179, 181, 241)),
     Category(title: 'Honey-Pancake', iconPath: 'icons/honey-pancakes.svg', color: const Color.fromARGB(255, 179, 181, 241)),
-    // Ajouter d'autres catégories si nécessaire
   ];
 
   // Données fictives pour les plats
@@ -48,16 +45,12 @@ class _HomeScreenState extends State<HomeScreen> {
     Dish(name: 'Canai Bread', description: 'Easy | 20mins | 230kCal', iconPath: 'icons/canai-bread.svg', color: const Color.fromARGB(255, 243, 189, 226)),
     Dish(name: 'Veggie', description: 'Easy | 30mins | 180kCal', iconPath: 'icons/orange-snacks.svg', color: const Color.fromARGB(255, 162, 209, 247)),
     Dish(name: 'BBQ Chicken', description: 'Easy | 30mins | 180kCal', iconPath: 'icons/blueberry-pancake.svg', color: const Color.fromARGB(255, 224, 194, 215)),
-    // Ajouter d'autres plats si nécessaire
   ];
 
   List<Category> filteredCategories = [];
-  List<Dish> filteredDishes = [];
   bool hasSearched = false; // Indique si une recherche a été effectuée
 
-
   void _logout() {
-    // Logique de déconnexion ici
     print("Logout");
   }
 
@@ -81,12 +74,13 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: const Color.fromARGB(255, 233, 231, 231),
-        title: const Text('BREAKFAST',
-        style: TextStyle(
-          fontWeight: FontWeight.w700,
-          fontFamily: "Poppins",
-          fontSize: 22,
-        ),
+        title: const Text(
+          'BREAKFAST',
+          style: TextStyle(
+            fontWeight: FontWeight.w700,
+            fontFamily: "Poppins",
+            fontSize: 22,
+          ),
         ),
         centerTitle: true,
         actions: [
@@ -102,26 +96,44 @@ class _HomeScreenState extends State<HomeScreen> {
         child: Column(
           children: [
             // Barre de recherche avec bouton
-            const SizedBox(height: 40),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 20.0),
+              child: TextField(
+                controller: _searchController,
+                onChanged: (value) {
+                  _search(); // Rechercher chaque fois que la valeur change
+                },
+                decoration: InputDecoration(
+                  hintText: "Search categories...",
+                  prefixIcon: Icon(Icons.search),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: BorderSide(color: Colors.grey),
+                  ),
+                ),
+              ),
+            ),
+
             // Titre Category
             const Align(
               alignment: Alignment.centerLeft,
               child: Text(
                 'Category',
                 style: TextStyle(
-                  fontSize: 22, 
+                  fontSize: 22,
                   fontFamily: "Poppins",
-                  fontWeight: FontWeight.w700
+                  fontWeight: FontWeight.w700,
                 ),
               ),
             ),
             const SizedBox(height: 10),
+
             // Liste horizontale des catégories
             SizedBox(
               height: 200,
               child: ListView.builder(
                 scrollDirection: Axis.horizontal,
-                physics: const BouncingScrollPhysics(), // Ajout d'un effet de rebond
+                physics: const BouncingScrollPhysics(),
                 itemCount: hasSearched ? filteredCategories.length : categories.length,
                 itemBuilder: (context, index) {
                   final category = hasSearched ? filteredCategories[index] : categories[index];
@@ -138,19 +150,21 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
             ),
             const SizedBox(height: 30),
+
             // Titre Dishes
             const Align(
               alignment: Alignment.centerLeft,
               child: Text(
                 'Recommandations de plats',
                 style: TextStyle(
-                  fontSize: 22, 
+                  fontSize: 22,
                   fontFamily: "Poppins",
-                  fontWeight: FontWeight.w700
+                  fontWeight: FontWeight.w700,
                 ),
               ),
             ),
             const SizedBox(height: 15),
+
             // Liste horizontale des plats
             SizedBox(
               height: 290,
@@ -160,11 +174,23 @@ class _HomeScreenState extends State<HomeScreen> {
                 itemBuilder: (context, index) {
                   return Padding(
                     padding: const EdgeInsets.only(right: 15.0),
-                    child: DishCard(
-                      dish: dishes[index],
-                      onView: () {
-                        print("View dish: ${dishes[index].name}");
-                      },
+                    child: Column(
+                      children: [
+                        DishCard(
+                          dish: dishes[index],
+                          onView: () {
+                            print("View dish: ${dishes[index].name}");
+                          },
+                        ),
+                        const SizedBox(height: 10),
+                        ElevatedButton(
+                          onPressed: () {
+                            print("Viewing ${dishes[index].name}");
+                            // Ajouter la logique pour afficher plus de détails sur le plat ici
+                          },
+                          child: const Text("View"),
+                        ),
+                      ],
                     ),
                   );
                 },
@@ -175,8 +201,6 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       // Bottom Navigation Bar avec trois icônes
       bottomNavigationBar: BottomNavigationBar(
-        // currentIndex: _selectedIndex,
-        // onTap: _onItemTapped,
         items: const [
           BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
           BottomNavigationBarItem(icon: Icon(Icons.notifications), label: 'Notifications'),
@@ -187,7 +211,7 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 }
 
-class DishCard extends StatelessWidget { // Modifié pour être un Widget Stateless
+class DishCard extends StatelessWidget {
   final Dish dish;
   final VoidCallback onView;
 
@@ -202,85 +226,83 @@ class DishCard extends StatelessWidget { // Modifié pour être un Widget Statel
       onTap: onView,
       child: FlipCard(
         flipOnTouch: true,
-      front: Card(
-        color: dish.color,
-        elevation: 3,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10),
+        front: Card(
+          color: dish.color,
+          elevation: 3,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: SizedBox(
+            width: 160, // Card légèrement agrandi
+            height: 270, // Card légèrement agrandi
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 8.0),
+                    child: SvgPicture.asset(
+                      dish.iconPath,
+                      width: 40, // SVG un peu plus petit
+                      height: 40,
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                  const SizedBox(height: 25),
+                  Text(
+                    dish.name,
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontFamily: "Poppins",
+                      fontWeight: FontWeight.w400,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+            ),
+          ),
         ),
-        child: SizedBox(
-          width: 160, // Card légèrement agrandi
-          height: 270, // Card légèrement agrandi
-          child: Padding(
-            padding: const EdgeInsets.all(12.0), // Plus d'espacement à l'intérieur
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                // Affichage du SVG, réduit un peu
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 8.0),
-                  child: SvgPicture.asset(
-                    dish.iconPath,
-                    width: 40, // SVG un peu plus petit
-                    height: 40,
-                    fit: BoxFit.contain,
+        back: Card(
+          color: dish.color,
+          elevation: 3,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: SizedBox(
+            width: 160,
+            height: 270,
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 8.0),
+                    child: SvgPicture.asset(
+                      dish.iconPath,
+                      width: 40,
+                      height: 40,
+                      fit: BoxFit.contain,
+                    ),
                   ),
-                ),
-                const SizedBox(height: 25),
-                // Titre de la catégorie
-                Text(
-                  dish.name,
-                  style: const TextStyle(
-                    fontSize: 16,
-                    fontFamily: "Poppins",
-                    fontWeight: FontWeight.w400
+                  const SizedBox(height: 25),
+                  Text(
+                    dish.description,
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontFamily: "Poppins",
+                      fontWeight: FontWeight.w400,
+                    ),
+                    textAlign: TextAlign.center,
                   ),
-                  textAlign: TextAlign.center,
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
       ),
-      back: Card(
-        color:dish.color ,
-        elevation: 3,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10),
-        ),
-        child: SizedBox(
-          width: 160,
-          height: 270,
-          child: Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 8.0),
-                  child: SvgPicture.asset(
-                    dish.iconPath,
-                    width: 40,
-                    height: 40,
-                    fit: BoxFit.contain,
-                  ),
-                ),
-                const SizedBox(height: 25),
-                Text(
-                  dish.description,
-                  style: const TextStyle(
-                    fontSize: 16,
-                    fontFamily: "Poppins",
-                    fontWeight: FontWeight.w400
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    )
     );
   }
 }
